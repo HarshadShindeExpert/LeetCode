1d2296fddba443e88d31c0195abfcc252a9243af
Harshad Shinde <shindeharshad2907@gmail.com>
2026-09-02 22:19:52 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -3,6 +3,9 @@ public:
     bool uniformArray(vector<int>& nums1) {
         return true ;
         // if any nmber in nums1 is odd then nums2 will contain all odd numbers .
+        // because if number is odd then keep it and if number is even then substract from odd number .
+
+        
         // if all numbers in nums1 are even then nums2 will contain all even numbers .
         
     }
